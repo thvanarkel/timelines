fec08a86a48c644a91125f84e189362d99214fff
thvanarkel <me@thomasvanarkel.nl>
2019-10-14 14:19:21 +0200

    Implemented titlebar

--- src/main.js
+++ src/main.js
@@ -20,9 +20,6 @@ const createMainWindow = () => {
   });
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
 };
